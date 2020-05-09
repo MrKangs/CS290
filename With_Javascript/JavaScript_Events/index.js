c823db19fe06a6a44ea7dbcce6b2674c0f6fa104
Mr.Kangs <gykang00@gmail.com>
2020-05-09 02:46:34 -0700

    JavaScript Complete

--- With_Javascript/JavaScript_Events/index.js
+++ With_Javascript/JavaScript_Events/index.js
@@ -5,16 +5,50 @@ window.addEventListener('click', function(){
 
 var buttons = document.getElementsByClassName("in-box-button");
 for(var i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener('click', function() /* or buttonClickListener*/  {
+    buttons[i].addEventListener('click', /*function() or */ buttonClickListener /*  {
         console.log(" == A button was clicked");
-    });
+    }*/);
 }
+// The /* */ is part of the function() section
 // This will add an event into the button that will print the string values into the console
 // Note that it will print "The window was clicked" and "A button was clicked" will be printed out
 // because the button is under the click listener
 
-function buttonClickListener(){
-    console.log(" == A button was clicked");
+function buttonClickListener(event){
+    console.log(" == A button was clicked, event:", event);
+    var button = event.currentTarget;
+    var box = button.parentNode;
+    box.classList.toggle("highlighted");
 }
 // This function was in the function from the for loop that can be use for references
+// The event will tell you what kind of event, what keyboard key was pressed, what location on the web was click, and more
+// the toggle function will do that if the class has the "", then it will remove it. 
+// If not, then add the class to the element
 
+
+var boxes = document.getElementsByClassName("box");
+for(var i = 0; i < boxes.length; i++){
+    boxes[i].addEventListener('click', function(event) {
+        console.log(" == A box was clicked");
+        console.log(" == event.target:", event.target);
+        console.log(" == event.currentTarget:", event.currentTarget);
+        event.stopPropagation();
+    });
+}
+// If we click the button in the box, note that there is a different between target verse currentTarget
+// While the currentTarget is the box, the target will be the button when we click the button
+// The button is the sub under the box while the box is the sub of window
+// Because there is click listener for both box, button, and window, we can see that all the event will occur when we select the button
+// currentTarget will the one that will only listen to the element that contains that event while the target will listen the original event
+// The event.stopPropagation() will stop event of the behavior of clicking
+// If there was not the event.stopPropagation(), then it will do the event of clicking the window
+
+
+var link = document.getElementById("website-link");
+link.addEventListener("click", function(event){
+    console.log(" == The link was clicked");
+    event.preventDefault();
+});
+// When we add the event.preventDefault(), it will stop the usual behavior of doing that event
+// In this case, when we click the link, usually we get a new tab of the link
+// while the event.preventDefault will only print the == The link was clicked on the console
--- With_Javascript/JavaScript_Events/wordBoxes.js
+++ With_Javascript/JavaScript_Events/wordBoxes.js
@@ -2,11 +2,24 @@ var allWords = [];
 var currentWord = 0;
 
 function handleNewWordsEntered(event) {
-  var text = '';
+  var text = event.currentTarget.value;
+  // This will get the current text input value
   allWords = text.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '')
     .toLowerCase().split(' ');
   currentWord = 0;
+  console.log(" == Event was triggered, text:", text);
+  console.log(" -- allWords", allWords);
 }
+// This function will replace all the symbol as space since we are only dealing with words
+
+var wordsInputElement = document.getElementById("words-input");
+
+//wordsInputElement.addEventListener('input', handleNewWordsEntered);
+// Setting up like this will always will trigger the event as you see in the console and line 9
+
+wordsInputElement.addEventListener('change', handleNewWordsEntered);
+// This will only trigger when it out of focus 
+
 
 function generateWordElem(word, highlightColor) {
 	var wordElem = document.createElement('span');
@@ -18,3 +31,86 @@ function generateWordElem(word, highlightColor) {
   }
   return wordElem;
 }
+
+
+
+
+/* The complete Code for Javascript is here */
+/*
+var allWords = [];
+var currentWord = 0;
+
+function handleWordsChange(event) {
+	var wordsText = event.currentTarget.value;
+  allWords = wordsText.replace(/[!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~]/g, '').toLowerCase().split(' ');
+  currentWord = 0;
+}
+
+function generateWordElem(word, highlightColor) {
+	var wordElem = document.createElement('span');
+  wordElem.classList.add('word');
+  wordElem.textContent = word;
+  if (highlightColor) {
+  	wordElem.classList.add('highlight');
+  	wordElem.classList.add(highlightColor);
+  }
+  
+  return wordElem;
+}
+
+function getHighlightColor() {
+	var selectedHighlightColorInput = document.querySelector('input[name="highlight-color"]:checked');
+  return selectedHighlightColorInput.value;
+}
+
+function handleAddWordButtonClick(event) {
+	
+  var word = allWords[currentWord];
+  
+  if (word) {
+  	var everyNthSelect = document.getElementById('every-nth-select');
+  	var everyNth = parseInt(everyNthSelect.value);
+  	var highlightColor = null;
+  
+    if (!((currentWord + 1) % everyNth)) {
+      highlightColor = getHighlightColor();
+    }
+
+    var wordElem = generateWordElem(word, highlightColor);
+
+    var container = event.currentTarget.parentNode.parentNode;
+    var wordsContainer = container.querySelector('.words-container');
+
+    wordsContainer.appendChild(wordElem);
+    currentWord = (currentWord + 1) % allWords.length;
+  }
+  
+}
+
+function handleWordsContainerClick(event) {
+	
+  var wordsContainer = event.currentTarget;
+  var clickedElem = event.target;
+ 
+	if (clickedElem.classList.contains('word')) {
+  	wordsContainer.removeChild(clickedElem);
+  } else if (clickedElem.parentNode.classList.contains('word')) {
+  	wordsContainer.removeChild(clickedElem.parentNode);
+  }
+  
+}
+
+var wordsInput = document.getElementById('words-input');
+wordsInput.addEventListener('change', handleWordsChange);
+
+var addWordButtons = document.getElementsByClassName('add-word-button');
+for (var i = 0; i < addWordButtons.length; i++) {
+	addWordButtons[i].addEventListener('click', handleAddWordButtonClick);
+}
+
+var wordsContainers = document.getElementsByClassName('words-container');
+for (var i = 0; i < wordsContainers.length; i++) {
+	wordsContainers[i].addEventListener('click', handleWordsContainerClick)
+} 
+
+*/
