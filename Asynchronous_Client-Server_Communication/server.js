0fda315d6901b43c04590c7155330fe419549ecd
MrKangs <gykang00@gmail.com>
2020-06-12 00:48:32 -0700

    Just Need to add comments, but other than that, it is done

--- Asynchronous_Client-Server_Communication/server.js
+++ Asynchronous_Client-Server_Communication/server.js
@@ -65,3 +65,6 @@ app.get('*', function (req, res, next) {
 app.listen(port, function () {
   console.log("== Server listening on port", port);
 })
+
+
+//Reference: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
